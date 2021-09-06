7c2b0d6d5193a8a47c932fca2423fcb503903288
Wesley <wesleycariutaba@gmail.com>
2021-09-06 11:42:04 -0300

    adjustment

--- UniversitySystem/src/br/edu/ufca/Modelo/IRepositorioAlunos.java
+++ UniversitySystem/src/br/edu/ufca/Modelo/IRepositorioAlunos.java
@@ -1,13 +1,13 @@
 package br.edu.ufca.Modelo;
 
 public interface IRepositorioAlunos {
-	void adicionar(Aluno discente);
+	public void adicionar(Aluno discente);
 		
-	void remover(Aluno discente);
+	public void remover(Aluno discente);
 	
-	Aluno consultar(String nome);
+	public Aluno consultar(String nome);
 	
-	int existe(Aluno discente);
+	public int existe(Aluno discente);
 	
 	public void atualizar(Aluno discente, String nome, int matricula, int idade);
 		
